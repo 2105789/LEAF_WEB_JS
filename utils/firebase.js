d5f5fb9f91775e78c1eb25b16ee51fc023cf46b3
87nehal <164513409+2105789@users.noreply.github.com>
2025-02-27 12:31:39 +0530

    firebase fix

--- utils/firebase.js
+++ utils/firebase.js
@@ -3,14 +3,23 @@ import { initializeApp, getApps } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAO_QqSP41SSxNP4I1gx5wvpRYojHocrDk",
-  authDomain: "leaf-4974f.firebaseapp.com",
-  projectId: "leaf-4974f",
-  storageBucket: "leaf-4974f.firebasestorage.app",
-  messagingSenderId: "333381157932",
-  appId: "1:333381157932:web:8aa88070347d913030bd89",
-  measurementId: "G-VYEYFPBZKX"
-}
+
+  apiKey: "AIzaSyC1-B9QAr3kuFjvOWKJSp52ngLg8m__3b8",
+
+  authDomain: "leafo-20e95.firebaseapp.com",
+
+  projectId: "leafo-20e95",
+
+  storageBucket: "leafo-20e95.firebasestorage.app",
+
+  messagingSenderId: "306355347618",
+
+  appId: "1:306355347618:web:9b757833f87082e216f7fb",
+
+  measurementId: "G-KC9YCP8DLL"
+
+};
+
 
 // Initialize Firebase
 let app
